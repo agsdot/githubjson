ee3412b6863b55f0009428b2bd5cb0d3a442e2eb
Jonathan Lai <jonathanlai@eltux.net>
2013-05-18 22:05:56 -0700

    Added rspec tests

--- spec/controllers/octoutput_controller_spec.rb
+++ spec/controllers/octoutput_controller_spec.rb
@@ -1,9 +1,34 @@
 require 'spec_helper'
 
 describe OctoutputController do
-  it "should be test" do
-    puts "i am a test"
-    expect(true).to eq true
+  # it "should be test" do
+  #   puts "i am a test"
+  #   expect(true).to eq true
+  # end
+
+  it "responds successfully to morris with an HTTP 200 status code" do
+    get :morris
+    expect(response).to be_success
+    expect(response.status).to eq 200
   end
+
+  # it "returns a JSON object" do
+  #   get '/morris.json'
+  #   response(response).to eq 'json'
+  # end
+  it "responds successfully to statusboard with an HTTP 200 status code" do
+    get :statusboard
+    expect(response).to be_success
+    expect(response.status).to eq 200
+  end
+
+  it "gets a json hash for statusboard" do
+    get :statusboard
+    commits = Octoutput.new
+    json = commits.fetch_commits
+    expect(json).to be_a Hash
+  end
+
+
 
 end
