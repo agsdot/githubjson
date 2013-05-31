f440b487b05b6d5407db283d51b11df19c23a41f
Jonathan Lai <jonathanlai@eltux.net>
2013-05-30 23:51:38 -0700

    Spruced up View styling of morris a bit, and pointed project to agsdot/mprss

--- app/models/octoutput.rb
+++ app/models/octoutput.rb
@@ -3,7 +3,7 @@ class Octoutput
 
   def fetch_commits
     client = Octokit::Client.new(:login => "#{ENV['GITHUB_USERNAME']}", :oauth_token => "#{ENV['GITHUB_OAUTH_TOKEN']}")
-    c = client.commits("agsdot/gaMapQuery")
+    c = client.commits("agsdot/mprss")
     json = {'Monday' => 0, 'Tuesday' => 0, 'Wednesday' => 0, 'Thursday' => 0, 'Friday' => 0, 'Saturday' => 0, 'Sunday' => 0}
     c.each do |item|
       date = item['commit']['committer']['date']
